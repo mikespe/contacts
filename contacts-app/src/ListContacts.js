74d7b3e1fb28cdce65901e8fb02e14c6c1cfb34e
Michael Speranza <mikespe34@yahoo.com>
2017-07-05 11:17:38 -0400

    feat: added avatar pics
    added pictures to corresponding names

--- contacts-app/src/ListContacts.js
+++ contacts-app/src/ListContacts.js
@@ -5,8 +5,10 @@ class ListContacts extends Component {
     return (
       <ol className='contacts-list'>
         {this.props.contacts.map( (contacts) => (
-          <li key={contacts.id}>
-            {contacts.name}
+          <li key={contacts.id} className='contact=list-item'>
+            <div className='contact-avatar' style={{
+                backgroundImage: `url(${contacts.avatarURL})`
+              }}/>
           </li>
         ))}
       </ol>
