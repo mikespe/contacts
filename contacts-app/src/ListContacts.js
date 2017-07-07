bd92cc2d33e8b894bfa9f09af360e356de0c4534
Michael Speranza <mikespe34@yahoo.com>
2017-07-07 11:29:33 -0400

    feat: connected search to ui - changed listcontacts from stateless component to controlled component, added updatequery function

--- contacts-app/src/ListContacts.js
+++ contacts-app/src/ListContacts.js
@@ -1,19 +1,55 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 
 class ListContacts extends Component {
-  render () {
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+  }
+
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() })
+  }
+
+  render() {
     return (
+      <div className='list-contacts'>
+        <div className='list-contacts-top'>
+          <input
+            className='search-contacts'
+            type='text'
+            placeholder='Search contacts'
+            value={this.state.query}
+            onChange={(event) => this.updateQuery(event.target.value)}
+            />
+        </div>
       <ol className='contacts-list'>
         {this.props.contacts.map( (contacts) => (
-          <li key={contacts.id} className='contact=list-item'>
+          <li key={contacts.id} className='contact-list-item'>
             <div className='contact-avatar' style={{
                 backgroundImage: `url(${contacts.avatarURL})`
               }}/>
+            <div className='contact-details'>
+              <p>{contacts.name}</p>
+              <p>{contacts.email}</p>
+            </div>
+            <button onClick={() => this.props.onDeleteContact(contacts)} className='contact-remove'>
+              Remove
+            </button>
           </li>
         ))}
       </ol>
+      </div>
     )
   }
+}
+
+ListContacts.propTypes = {
+
 }
 
 export default ListContacts
